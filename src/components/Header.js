a9f0f9f722ffa9110ebca7533ed6192eb7ada5c7
UssefBhl <ybhl83@gmail.com>
2021-08-28 02:18:22 +0100

    ✨ 💄 added bootstrap and almost finished header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
-
+import calculatorLogo from '../images/calculator.svg';
 const Header = ({ title, backColor }) => {
     return (
         <header className='header' style={{'background-color' : backColor}}>
-            <div className='content header-content'>
+            <div className='content header-content align-items-center px-4'>
                 <p className='title'>{title}</p>
+                <div className='d-flex mx-auto'>
+                    <img src={calculatorLogo} alt="logo" className='middleLogo' />
+                    <p className='cl-second'>React-calculator</p>
+                </div>
             </div>
         </header>
     )
