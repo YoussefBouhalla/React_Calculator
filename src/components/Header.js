03a484fec9ae8dc43a20f2d47faf0fd915054e5f
UssefBhl <ybhl83@gmail.com>
2021-08-28 02:57:36 +0100

    💄 finished the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import calculatorLogo from '../images/calculator.svg';
 const Header = ({ title, backColor }) => {
     return (
-        <header className='header' style={{'background-color' : backColor}}>
+        <header className='header bxs-header' style={{'background-color' : backColor}}>
             <div className='content header-content align-items-center px-4'>
                 <p className='title'>{title}</p>
                 <div className='d-flex mx-auto'>
                     <img src={calculatorLogo} alt="logo" className='middleLogo' />
-                    <p className='cl-second'>React-calculator</p>
+                    <p className='cl-second fs-16 fw-500'>React-calculator</p>
                 </div>
             </div>
         </header>
