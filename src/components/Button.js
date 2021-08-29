60c82fd5ddea2937d4e1f8c4721883f9f835a1bd
UssefBhl <ybhl83@gmail.com>
2021-08-29 22:22:06 +0100

    💄 finished calculator UI

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import backspace from '../images/backspace.svg'
 
-function Button({ backColor,frontColor }) {
+function Button({ backColor,frontColor,title }) {
+    let btitle;
+    if (title == 'back') {
+        btitle = <img src={backspace} alt="back" className='backspace' />;
+    }else{
+        btitle = title;
+    }
     return (
         <div className='keypad-button' style={{backgroundColor : backColor}}>
-            <button className='key text-center fs-18 fw-600' style={{backgroundColor : frontColor}}>
-                1
+            <button className='key text-center fs-18 fw-600 cl-primary' style={{backgroundColor : frontColor}}>
+                {btitle}
             </button>
         </div>
     )
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -5,36 +5,36 @@ function Keypad() {
     return (
         <div className='keypad mx-auto'>
             <div className='complex-ops d-flex flex-wrap justify-content-between align-content-between'>
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
+                <Button backColor={'#BE872D'} frontColor={'#FEC260'} title={'√'} />
+                <Button backColor={'#BE872D'} frontColor={'#FEC260'} title={'('} />
+                <Button backColor={'#BE872D'} frontColor={'#FEC260'} title={')'} />
+                <Button backColor={'#BE872D'} frontColor={'#FEC260'} title={'^'} />
+                <Button backColor={'#BE872D'} frontColor={'#FEC260'} title={'ln'} />
+                <Button backColor={'#BE872D'} frontColor={'#FEC260'} title={'x!'} />
             </div>
-            <div className='clear'>
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
+            <div className='clear d-flex flex-wrap justify-content-between align-content-between'>
+                <Button backColor={'#BE872D'} frontColor={'#FEC260'} title={'back'} />
+                <Button backColor={'#2D76BE'} frontColor={'#609FFE'} title={'C'} />
             </div>
-            <div className='numbers'>
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
+            <div className='numbers  d-flex flex-wrap justify-content-between align-content-between'>
+                <Button backColor={'#AA7928'} frontColor={'#D9A44E'} title={'1'} />
+                <Button backColor={'#AA7928'} frontColor={'#D9A44E'} title={'2'} />
+                <Button backColor={'#AA7928'} frontColor={'#D9A44E'} title={'3'} />
+                <Button backColor={'#AA7928'} frontColor={'#D9A44E'} title={'4'} />
+                <Button backColor={'#AA7928'} frontColor={'#D9A44E'} title={'5'} />
+                <Button backColor={'#AA7928'} frontColor={'#D9A44E'} title={'6'} />
+                <Button backColor={'#AA7928'} frontColor={'#D9A44E'} title={'7'} />
+                <Button backColor={'#AA7928'} frontColor={'#D9A44E'} title={'8'} />
+                <Button backColor={'#AA7928'} frontColor={'#D9A44E'} title={'9'} />
+                <Button backColor={'#AA7928'} frontColor={'#D9A44E'} title={'0'} />
+                <Button backColor={'#AA7928'} frontColor={'#D9A44E'} title={'.'} />
+                <Button backColor={'#C53B3B'} frontColor={'#FE6565'} title={'='} />
             </div>
-            <div className='operations'>
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
-                <Button backColor={'#BE872D'} frontColor={'#FEC260'} />
+            <div className='operations  d-flex flex-wrap justify-content-between align-content-between'>
+                <Button backColor={'#BE872D'} frontColor={'#FEC260'} title={'+'} />
+                <Button backColor={'#BE872D'} frontColor={'#FEC260'} title={'-'} />
+                <Button backColor={'#BE872D'} frontColor={'#FEC260'} title={'×'} />
+                <Button backColor={'#BE872D'} frontColor={'#FEC260'} title={'÷'} />
             </div>
         </div>
     )
